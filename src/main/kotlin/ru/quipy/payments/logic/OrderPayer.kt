5ce8ba76c49f55e5ca871303ff64226899a1489e
Margarita Skokova <367538@niuitmo.ru>
2026-02-26 20:17:36 +0300

    try

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -54,13 +54,12 @@ class OrderPayer {
             throw RateLimitExceededException()
         }
 
-        val createdEvent = paymentESService.create {
-            it.create(paymentId, orderId, amount)
-        }
-        logger.trace("Payment ${createdEvent.paymentId} for order $orderId created.")
-
         scope.launch {
             try {
+                val createdEvent = paymentESService.create {
+                    it.create(paymentId, orderId, amount)
+                }
+                logger.trace("Payment ${createdEvent.paymentId} for order $orderId created.")
                 paymentService.submitPaymentRequest(paymentId, amount, createdAt, deadline)
             } catch (ex: Exception) {
                 logger.error("Payment submission failed for $paymentId", ex)
